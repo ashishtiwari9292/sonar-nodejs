a3c8ab15b0c9f406146d14140ed9b88894de7502
ashishtiwari9292 <ashishtiwari9292@gmail.com>
2024-08-08 11:37:11 +0530

    minnor fix

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.93.66.84:9000/',
+  serverUrl: 'http://54.91.1.55:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
